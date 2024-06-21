2d0e37ad4654fa913714be182801c9bf74c0d10e
HelgaZhizhka <helgazhyzhka@gmail.com>
2024-06-21 20:37:47 +0200

    fix: refactor enau mRoutePath to object

--- packages/client/src/core/Routes.enum.ts
+++ packages/client/src/core/Routes.enum.ts
@@ -1,13 +1,15 @@
-export enum RoutePath {
-  HOME = '/',
-  GAME = '/game',
-  ERROR = '*',
-  SIGN_IN = '/signin',
-  SIGN_UP = '/signup',
-  PROFILE = '/profile',
-  LEADER_BOARD = '/leaderboard',
-  FORUM = '/forum',
-  FORUM_TOPIC = '/forum:id',
-  PAGE_NOT_FOUND = '/404',
-  SERVER_ERROR = '/500',
-}
+export const RoutePath = {
+  HOME: '/',
+  GAME: '/game',
+  ERROR: '*',
+  SIGN_IN: '/signin',
+  SIGN_UP: '/signup',
+  PROFILE: '/profile',
+  LEADER_BOARD: '/leaderboard',
+  FORUM: '/forum',
+  FORUM_TOPIC: '/forum:id',
+  PAGE_NOT_FOUND: '/404',
+  SERVER_ERROR: '/500',
+} as const
+
+export type RoutePathType = typeof RoutePath[keyof typeof RoutePath]
