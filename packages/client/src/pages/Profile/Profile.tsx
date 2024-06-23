15ac6a464eb5ccf563358059c5afbecdea95040e
HelgaZhizhka <helgazhyzhka@gmail.com>
2024-06-23 21:01:36 +0200

    feat: install Formik library

--- packages/client/src/pages/Profile/Profile.tsx
+++ packages/client/src/pages/Profile/Profile.tsx
@@ -1,6 +1,46 @@
+import { Formik, Field, Form, FormikHelpers } from 'formik'
+
+interface Values {
+  firstName: string
+  lastName: string
+  email: string
+}
+
+const initialValues: Values = {
+  firstName: '',
+  lastName: '',
+  email: '',
+}
+
 const Profile = () => (
   <>
     <h1>Profile page</h1>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+        setTimeout(() => {
+          console.log(JSON.stringify(values, null, 2))
+          setSubmitting(false)
+        }, 500)
+      }}>
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field id="firstName" name="firstName" placeholder="John" />
+
+        <label htmlFor="lastName">Last Name</label>
+        <Field id="lastName" name="lastName" placeholder="Doe" />
+
+        <label htmlFor="email">Email</label>
+        <Field
+          id="email"
+          name="email"
+          placeholder="john@acme.com"
+          type="email"
+        />
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   </>
 )
 
