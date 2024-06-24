0a2fdd9562246f7324cd4b0d3f70cc3f5bc7fc95
evgeniya-burlachenko <e.accounted@gmail.com>
2024-06-24 22:22:39 +0300

    39-19 fixed after review

--- packages/client/src/pages/Forum/Forum.tsx
+++ packages/client/src/pages/Forum/Forum.tsx
@@ -1,9 +1,25 @@
-import { RoutePath } from '@/core/Routes.enum'
 import { LikeOutlined } from '@ant-design/icons'
-import { Button, Input, Modal, Popover, Space, Table } from 'antd'
-import TextArea from 'antd/es/input/TextArea'
+import { Button, Popover, Space } from 'antd'
 import { ChangeEvent, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { mockData } from './mockData'
+import TopicModal from './TopicModal/TopicModal'
+import CommentModal from './CommentModal/CommentModal'
+import ForumList from './ForumList/ForumList'
+import TopicDetails from './TopicDetails/TopicDetails'
+
+export type Comment = {
+  key: string
+  comment: string
+  emoji: string
+}
+
+export type Topic = {
+  key: string
+  name: string
+  commentsCount: string
+  content: string
+  comments: Comment[]
+}
 
 const Forum = () => {
   const [isModalVisible, setIsModalVisible] = useState(false)
@@ -11,63 +27,18 @@ const Forum = () => {
   const [newTopicName, setNewTopicName] = useState('')
   const [newTopicContent, setNewTopicContent] = useState('')
   const [newComment, setNewComment] = useState('')
-  const [dataSource, setDataSource] = useState<any[]>([])
-  const [selectedTopic, setSelectedTopic] = useState<any>(null)
-  const [selectedComments, setSelectedComments] = useState<any[]>([])
+  const [dataSource, setDataSource] = useState<Topic[]>([])
+  //TODO: убрать any
+  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null)
+  const [selectedComments, setSelectedComments] = useState<Comment[]>([])
 
-  const navigate = useNavigate()
   useEffect(() => {
     const fetchData = async () => {
-      const data = [
-        {
-          key: '1',
-          name: 'New games',
-          commentsCount: '3',
-          content: 'This is the content of New games topic',
-          comments: [
-            { key: '1', comment: 'interesting', emoji: '' },
-            { key: '2', comment: 'no bad', emoji: '' },
-            { key: '3', comment: 'i like it', emoji: '' },
-          ],
-        },
-        {
-          key: '2',
-          name: 'Gamedev',
-          commentsCount: '3',
-          content: 'This is the content of Gamedev topic',
-          comments: [
-            { key: '1', comment: 'ok', emoji: '' },
-            { key: '2', comment: 'chudesno', emoji: '' },
-            { key: '3', comment: 'super', emoji: '' },
-          ],
-        },
-        {
-          key: '3',
-          name: 'Game design',
-          commentsCount: '3',
-          content: 'This is the content of Game design topic',
-          comments: [
-            { key: '1', comment: 'i love it', emoji: '' },
-            { key: '2', comment: 'its ok', emoji: '' },
-            { key: '3', comment: 'eee', emoji: '' },
-          ],
-        },
-        {
-          key: '4',
-          name: 'Technologies',
-          commentsCount: '3',
-          content: 'This is the content of Technologies topic',
-          comments: [
-            { key: '1', comment: 'ok', emoji: '' },
-            { key: '2', comment: 'eee', emoji: '' },
-            { key: '3', comment: 'amazing', emoji: '' },
-          ],
-        },
-      ]
+      const data = mockData
       setDataSource(data)
     }
     fetchData()
-  }, [])
+  }, [dataSource])
 
   const showModal = () => {
     setIsModalVisible(true)
@@ -97,17 +68,18 @@ const Forum = () => {
   }
 
   const handleCommentOk = () => {
+    if (!selectedTopic) return
     const newKey = (selectedComments.length + 1).toString()
     const newCommentEntry = { key: newKey, comment: newComment, emoji: '' }
-    const updatedDataSource = dataSource.map(item =>
-      item.key === selectedTopic.key
+    const updatedDataSource = dataSource.map(item => {
+      return item.key === selectedTopic.key
         ? {
             ...item,
             comments: [...item.comments, newCommentEntry],
             commentsCount: (parseInt(item.commentsCount) + 1).toString(),
           }
         : item
-    )
+    })
     setDataSource(updatedDataSource)
     setSelectedComments([...selectedComments, newCommentEntry])
     setIsCommentModalVisible(false)
@@ -130,12 +102,13 @@ const Forum = () => {
     setNewComment(e.target.value)
   }
 
-  const handleNameClick = (record: any) => {
+  const handleNameClick = (record: Topic) => {
     setSelectedTopic(record)
     setSelectedComments(record.comments)
   }
 
   const handleEmojiSelect = (commentKey: string, emoji: string) => {
+    if (!selectedTopic) return
     const updatedComments = selectedComments.map(comment =>
       comment.key === commentKey ? { ...comment, emoji } : comment
     )
@@ -175,9 +148,9 @@ const Forum = () => {
       title: 'NAME',
       dataIndex: 'name',
       key: 'name',
-      render: (text: string, record: any) => (
-        <a onClick={() => handleNameClick(record)}>{text}</a>
-      ),
+      render: (text: string, record: Topic) => {
+        return <a onClick={() => handleNameClick(record)}>{text}</a>
+      },
     },
     {
       title: 'COMMENTS',
@@ -191,7 +164,7 @@ const Forum = () => {
       title: 'COMMENT',
       dataIndex: 'comment',
       key: 'comment',
-      render: (_: string, record: any) => (
+      render: (_: string, record: Comment) => (
         <div>
           <div>{record.comment}</div>
           <div>
@@ -207,84 +180,39 @@ const Forum = () => {
     <div className="wrapper">
       <div className="forums">
         {!selectedTopic ? (
-          <>
-            <h1 className="forums__header">Forums</h1>
-            <Table
-              className="forums__table"
-              dataSource={dataSource}
-              columns={columns}
-            />
-            <div className="forums__btn">
-              <Button
-                className="nes-btn forums__btn-comments"
-                onClick={() => navigate(RoutePath.HOME)}>
-                Back
-              </Button>
-              <Button
-                className="nes-btn forums__btn-comments"
-                onClick={showModal}>
-                Add Topic
-              </Button>
-            </div>
-          </>
+          <ForumList
+            dataSource={dataSource}
+            columns={columns}
+            showModal={showModal}
+          />
         ) : (
-          <div>
-            <h2 className="forums__header">{selectedTopic.name}</h2>
-            <p>{selectedTopic.content}</p>
-            <Table
-              className="forums__table"
-              dataSource={selectedComments}
-              columns={commentsColumns}
-              showHeader={false}
-            />
-            <div className="forums__btn">
-              <Button
-                className="nes-btn forums__btn-games"
-                onClick={() => {
-                  setSelectedTopic(null)
-                  setSelectedComments([])
-                }}>
-                Back
-              </Button>
-              <Button
-                className="nes-btn forums__btn-comments"
-                onClick={showCommentModal}>
-                Add Comment
-              </Button>
-            </div>
-          </div>
+          <TopicDetails
+            selectedTopic={selectedTopic}
+            selectedComments={selectedComments}
+            commentsColumns={commentsColumns}
+            onBack={() => {
+              setSelectedTopic(null)
+              setSelectedComments([])
+            }}
+            showCommentModal={showCommentModal}
+          />
         )}
-
-        <Modal
-          title="New Topic"
-          okText="Add"
-          open={isModalVisible}
+        <TopicModal
+          visible={isModalVisible}
+          topicName={newTopicName}
+          topicContent={newTopicContent}
           onOk={handleOk}
-          onCancel={handleCancel}>
-          <Input
-            placeholder="Enter new topic name"
-            value={newTopicName}
-            onChange={handleTopicNameChange}
-          />
-          <TextArea
-            placeholder="Enter new topic content"
-            value={newTopicContent}
-            onChange={handleTopicContentChange}
-          />
-        </Modal>
-
-        <Modal
-          title="Add new comment"
-          open={isCommentModalVisible}
-          okText="Add"
+          onCancel={handleCancel}
+          onTopicNameChange={handleTopicNameChange}
+          onTopicContentChange={handleTopicContentChange}
+        />
+        <CommentModal
+          visible={isCommentModalVisible}
+          comment={newComment}
           onOk={handleCommentOk}
-          onCancel={handleCommentCancel}>
-          <TextArea
-            placeholder="New comment"
-            value={newComment}
-            onChange={handleCommentInputChange}
-          />
-        </Modal>
+          onCancel={handleCommentCancel}
+          onCommentChange={handleCommentInputChange}
+        />
       </div>
     </div>
   )
---
+++ packages/client/src/pages/Forum/ForumList/ForumList.tsx
@@ -0,0 +1,40 @@
+import { RoutePath } from '@/core/Routes.enum'
+import { Button, Table } from 'antd'
+import { FC } from 'react'
+import { useNavigate } from 'react-router'
+import styles from './ForumList.module.scss'
+import cn from 'classnames'
+import { Topic } from '../Forum'
+import { ColumnsType } from 'antd/es/table'
+
+type ForumListProps = {
+  dataSource: Topic[]
+  columns: ColumnsType
+  showModal: () => void
+}
+const ForumList: FC<ForumListProps> = ({ dataSource, columns, showModal }) => {
+  const navigate = useNavigate()
+  return (
+    <>
+      <h1 className={styles.forums__header}>Forums</h1>
+      <Table
+        className={styles.forums__table}
+        dataSource={dataSource}
+        columns={columns}
+      />
+      <div className={styles.forums__btn}>
+        <Button
+          className={cn(styles['forums__btn-comments'], 'nes-btn')}
+          onClick={() => navigate(RoutePath.HOME)}>
+          Back
+        </Button>
+        <Button
+          className={cn(styles['forums__btn-comments'], 'nes-btn')}
+          onClick={showModal}>
+          Add Topic
+        </Button>
+      </div>
+    </>
+  )
+}
+export default ForumList
