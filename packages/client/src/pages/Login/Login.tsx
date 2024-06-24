530b5462678b8039f5143512c7d926702749307f
Grosno <pbc20092yandex.ru>
2024-06-24 13:41:26 +0300

    Добавлен каркас логин страницы

--- packages/client/src/pages/Login/Login.tsx
+++ packages/client/src/pages/Login/Login.tsx
@@ -1,7 +1,18 @@
+import './Login.scss'
+
 const Login = () => (
-  <>
-    <h1>Login page</h1>
-  </>
+  <div className="container center login">
+    <div className="login-card">
+      <div className="login-card__header">
+        <div className="login-card__logo">
+          <img src="src/assets/images/logo_lg.svg" alt="Panda logo" />
+        </div>
+        <div className="login-card__title">Welcome back!</div>
+      </div>
+      <form action="" className="login-card__fields"></form>
+      <div className="login-card__buttons"></div>
+    </div>
+  </div>
 )
 
 export default Login
