af754d63b58b3afcc36431a63665966a8ae83dda
Nikita Potokin <116308322+NPotokin@users.noreply.github.com>
2024-06-26 13:24:18 +0300

    Feat/39 38 canvas game mechanic (#12)
    * inintal set up and most of the game logic
    
    * yarn.lock changes
    
    * basic mechanic with win and lose scenarios
    
    * Game is working but flickering
    
    * Struggle with requestAnimationFrame
    
    * fixed RaF by pre-loading images
    
    * updated jest to recognize png

--- packages/client/src/pages/Game/Game.tsx
+++ packages/client/src/pages/Game/Game.tsx
@@ -1,44 +1,330 @@
-import React, { useState } from 'react'
-import styles from './styles.module.scss'
+import React, { useRef, useEffect, useState, useCallback, useMemo } from 'react'
+import styles from './Game.module.scss'
+import { Positions } from './positions'
 import pandaStart from '@images/panda_start.svg'
 import pandaWin from '@images/panda_win.svg'
 import pandaLost from '@images/panda_over.svg'
+import pumpkin from '@images/pumpkin.png'
+import tiger from '@images/tiger.png'
+import rock from '@images/rock.png'
 import { GameModal } from '@/components'
 
 const Game: React.FC = () => {
+  // Канвас и препятствия
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const timerRef = useRef<NodeJS.Timeout | null>(null)
+  const obstacles = useMemo(() => Positions.obsticles, [])
+  const canvasSize = useMemo(() => ({ width: 800, height: 600 }), [])
+
+  // Стейты модалок
   const [isStartModalVisible, setIsStartModalVisible] = useState(true)
-  const [isGameVisible, setIsGameVisible] = useState(false)
   const [isGameOverVisible, setIsGameOverVisible] = useState(false)
   const [isGameWinVisible, setIsGameWinVisible] = useState(false)
 
+  // Стейты скора игры
+  const [time, setTime] = useState(0)
+  const [steps, setSteps] = useState(0)
+
+  // Стейты игрока, врагов и кристаллов
+  const [playerPosition, setPlayerPosition] = useState({ x: 0, y: 0 })
+  const [gems, setGems] = useState<{ x: number; y: number }[]>(Positions.gems)
+  const [enemies, setEnemies] = useState<{ x: number; y: number }[]>(
+    Positions.enemies
+  )
+
+  // Пре-прогружаем картинки - иначе RAF не отрабатывает
+  const [imagesLoaded, setImagesLoaded] = useState(false)
+  const [images, setImages] = useState({
+    rock: new Image(),
+    pumpkin: new Image(),
+    pandaWin: new Image(),
+    tiger: new Image(),
+  })
+
+  useEffect(() => {
+    const rockImg = new Image()
+    const pumpkinImg = new Image()
+    const pandaWinImg = new Image()
+    const tigerImg = new Image()
+
+    let loaded = 0
+    const total = 4
+
+    const onLoad = () => {
+      loaded += 1
+      if (loaded === total) {
+        setImagesLoaded(true)
+      }
+    }
+
+    rockImg.src = rock
+    pumpkinImg.src = pumpkin
+    pandaWinImg.src = pandaWin
+    tigerImg.src = tiger
+
+    rockImg.onload = onLoad
+    pumpkinImg.onload = onLoad
+    pandaWinImg.onload = onLoad
+    tigerImg.onload = onLoad
+
+    setImages({
+      rock: rockImg,
+      pumpkin: pumpkinImg,
+      pandaWin: pandaWinImg,
+      tiger: tigerImg,
+    })
+  }, [])
+
+  // Отрисовываем
+  const drawObstacles = useCallback(
+    (ctx: CanvasRenderingContext2D) => {
+      obstacles.forEach(obstacle => {
+        ctx.drawImage(
+          images.rock,
+          obstacle.x,
+          obstacle.y,
+          obstacle.width,
+          obstacle.height
+        )
+      })
+    },
+    [obstacles, images.rock]
+  )
+
+  const drawGems = useCallback(
+    (ctx: CanvasRenderingContext2D) => {
+      gems.forEach(gem => {
+        ctx.drawImage(images.pumpkin, gem.x, gem.y, 40, 40)
+      })
+    },
+    [gems, images.pumpkin]
+  )
+
+  const drawPlayer = useCallback(
+    (ctx: CanvasRenderingContext2D) => {
+      ctx.drawImage(images.pandaWin, playerPosition.x, playerPosition.y, 40, 40)
+    },
+    [playerPosition, images.pandaWin]
+  )
+
+  const drawEnemies = useCallback(
+    (ctx: CanvasRenderingContext2D) => {
+      enemies.forEach(enemy => {
+        ctx.drawImage(images.tiger, enemy.x, enemy.y, 40, 40)
+      })
+    },
+    [enemies, images.tiger]
+  )
+
+  // Хэндлеры модалок
   const handleStartModalButton = () => {
     setIsStartModalVisible(false)
-    setIsGameVisible(true)
     setIsGameWinVisible(false)
     setIsGameOverVisible(false)
+    setPlayerPosition({ x: 0, y: 0 })
+    setSteps(1) // хак, чтоб не прогружалось пустое поле
+    setTime(0)
+    setGems(Positions.gems)
+    setEnemies(Positions.enemies)
+
+    timerRef.current = setInterval(() => {
+      setTime(prevSeconds => prevSeconds + 1)
+    }, 1000)
   }
 
   const handleVictory = () => {
     setIsGameWinVisible(true)
-    setIsGameVisible(false)
+    if (timerRef.current) {
+      clearInterval(timerRef.current)
+    }
   }
 
   const handleDefeat = () => {
     setIsGameOverVisible(true)
-    setIsGameVisible(false)
+    if (timerRef.current) {
+      clearInterval(timerRef.current)
+    }
   }
+
+  // Хэндлер позиций игрока и врагов
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      setPlayerPosition(prev => {
+        let { x, y } = prev
+        const step = 20
+
+        switch (event.key) {
+          case 'ArrowUp':
+            y -= step
+            break
+          case 'ArrowDown':
+            y += step
+            break
+          case 'ArrowLeft':
+            x -= step
+            break
+          case 'ArrowRight':
+            x += step
+            break
+        }
+
+        const isCollidingWithEnemies = enemies.some(
+          enemy =>
+            x < enemy.x + 40 &&
+            x + 40 > enemy.x &&
+            y < enemy.y + 40 &&
+            y + 40 > enemy.y
+        )
+
+        if (isCollidingWithEnemies) {
+          handleDefeat()
+        }
+
+        const isCollidingWithObstaclesOrWalls =
+          obstacles.some(
+            obstacle =>
+              x < obstacle.x + obstacle.width &&
+              x + 40 > obstacle.x &&
+              y < obstacle.y + obstacle.height &&
+              y + 40 > obstacle.y
+          ) ||
+          x < 0 ||
+          x + 40 > canvasSize.width ||
+          y < 0 ||
+          y + 40 > canvasSize.height
+
+        if (isCollidingWithObstaclesOrWalls) {
+          return prev
+        }
+
+        const newGems = gems.filter(
+          gem =>
+            !(
+              x < gem.x + 40 &&
+              x + 40 > gem.x &&
+              y < gem.y + 40 &&
+              y + 40 > gem.y
+            )
+        )
+        setGems(newGems)
+
+        if (newGems.length === 0) {
+          handleVictory()
+        }
+
+        setSteps(steps + 1)
+
+        return { x, y }
+      })
+
+      setEnemies(prev =>
+        prev.map(enemy => {
+          let { x: enemyX, y: enemyY } = enemy
+          const step = 40
+          const directions = ['up', 'down', 'left', 'right']
+          const randomDirection =
+            directions[Math.floor(Math.random() * directions.length)]
+
+          switch (randomDirection) {
+            case 'up':
+              enemyY -= step
+              break
+            case 'down':
+              enemyY += step
+              break
+            case 'left':
+              enemyX -= step
+              break
+            case 'right':
+              enemyX += step
+              break
+          }
+
+          const isCollidingWithWalls =
+            enemyX < 0 ||
+            enemyX + 40 > canvasSize.width ||
+            enemyY < 0 ||
+            enemyY + 40 > canvasSize.height
+
+          const isCollidingWithGems = gems.some(
+            gem =>
+              enemyX < gem.x + 40 &&
+              enemyX + 40 > gem.x &&
+              enemyY < gem.y + 40 &&
+              enemyY + 40 > gem.y
+          )
+
+          const isCollidingWithObstacles = obstacles.some(
+            obstacle =>
+              enemyX < obstacle.x + obstacle.width &&
+              enemyX + 40 > obstacle.x &&
+              enemyY < obstacle.y + obstacle.height &&
+              enemyY + 40 > obstacle.y
+          )
+
+          const isCollidingWithPlayer =
+            playerPosition.x < enemyX + 40 &&
+            playerPosition.x + 40 > enemyX &&
+            playerPosition.y < enemyY + 40 &&
+            playerPosition.y + 40 > enemyY
+
+          if (
+            isCollidingWithWalls ||
+            isCollidingWithObstacles ||
+            isCollidingWithGems
+          ) {
+            return enemy
+          }
+
+          if (isCollidingWithPlayer) {
+            handleDefeat()
+          }
+
+          return { x: enemyX, y: enemyY }
+        })
+      )
+    },
+    [steps]
+  )
+
+  // Основной луп игры
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas?.getContext('2d')
+
+    const draw = () => {
+      if (context) {
+        context.clearRect(0, 0, canvasSize.width, canvasSize.height)
+        drawObstacles(context)
+        drawGems(context)
+        drawPlayer(context)
+        drawEnemies(context)
+        window.requestAnimationFrame(draw)
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+    window.requestAnimationFrame(draw)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [handleKeyDown])
 
   return (
     <div className={styles.game}>
-      {/* Temporary mock game div */}
-      {isGameVisible && (
-        <div>
-          <h1 className="nes-text is-disabled">This is temporary</h1>
-          <button onClick={handleDefeat}>Click to lose</button>
-          <button onClick={handleVictory}>Click to win</button>
-        </div>
-      )}
-      {/* Start game modal */}
+      {/* Игра */}
+      <div className={styles.canvas}>
+        <div>Steps: {steps}</div>
+        <div>Time: {time}</div>
+        <canvas
+          ref={canvasRef}
+          width={canvasSize.width}
+          height={canvasSize.height}
+        />
+      </div>
+
+      {/* Модалка старта */}
       <GameModal
         visible={isStartModalVisible}
         imageSrc={pandaStart}
@@ -47,7 +333,7 @@ const Game: React.FC = () => {
         onYesClick={handleStartModalButton}
       />
 
-      {/* Win game modal */}
+      {/* Модалка победы */}
       <GameModal
         visible={isGameWinVisible}
         imageSrc={pandaWin}
@@ -57,7 +343,7 @@ const Game: React.FC = () => {
         onYesClick={handleStartModalButton}
       />
 
-      {/* Lost game modal */}
+      {/* Модалка фиаско */}
       <GameModal
         visible={isGameOverVisible}
         imageSrc={pandaLost}
