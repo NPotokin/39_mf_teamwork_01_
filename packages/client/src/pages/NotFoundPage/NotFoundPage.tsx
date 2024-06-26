e1d737a19b996d7041f053426b731dda99967b52
Evgeniya Burlachenko <63720882+evgeniya-burlachenko@users.noreply.github.com>
2024-06-26 09:13:51 +0300

    39-15 верстка страницы ошибок 400 (#13)

--- packages/client/src/pages/NotFoundPage/NotFoundPage.tsx
+++ packages/client/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,32 @@
-const NotFoundPage = () => (
-  <>
-    <h1>Not Found page</h1>
-  </>
-)
+import { useNavigate } from 'react-router'
+import cn from 'classnames'
+import styles from './NotFoundPage.module.scss'
+import panda404 from '@images/panda404.svg'
+import { Button } from 'antd'
+import { RoutePath } from '@/core/Routes.enum'
+const NotFoundPage = () => {
+  const navigate = useNavigate()
+  return (
+    <div className={cn(styles.wrapper, 'page')}>
+      <div className={styles.pageError}>
+        <h1 className={styles.pageError__404}>
+          4
+          <img
+            src={panda404}
+            alt="panda"
+            className={styles[`pageError__500-img`]}
+          />
+          4
+        </h1>
+        <h1 className={styles.pageError__header}>page not found</h1>
+        <Button
+          className={cn(styles['pageError__btn'])}
+          onClick={() => navigate(RoutePath.HOME)}>
+          Go home
+        </Button>
+      </div>
+    </div>
+  )
+}
 
 export default NotFoundPage
