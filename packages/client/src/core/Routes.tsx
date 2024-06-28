eec036f67110ac431080820d75f43e3b319a58ff
evgeniya-burlachenko <e.accounted@gmail.com>
2024-06-28 18:35:06 +0300

    39-42 fixed Routes

--- packages/client/src/core/Routes.tsx
+++ packages/client/src/core/Routes.tsx
@@ -21,62 +21,63 @@ import { ForumProvider } from '@/context/ForumContext'
 
 const routes: RouteObject[] = [
   {
-    path: RoutePath.HOME,
-    element: <Home />,
-  },
-  {
-    path: RoutePath.GAME,
-    element: (
-      <ProtectedRoute>
-        <Game />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: RoutePath.SIGN_IN,
-    element: <Login />,
-  },
-  {
-    path: RoutePath.SIGN_UP,
-    element: <Registration />,
-  },
-  {
-    path: RoutePath.LEADER_BOARD,
-    element: (
-      <ProtectedRoute>
-        <LeaderBoard />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: RoutePath.FORUM,
-    element: (
-      <ProtectedRoute>
-        <ForumProvider>
-          <Forum />
-        </ForumProvider>
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: RoutePath.PROFILE,
-    element: (
-      <ProtectedRoute>
-        <Profile />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: RoutePath.PAGE_NOT_FOUND,
-    element: <NotFoundPage />,
-  },
-  {
-    path: RoutePath.SERVER_ERROR,
-    element: <ServerErrorPage />,
-  },
-  {
-    path: RoutePath.ERROR,
-    element: <NotFoundPage />,
+    element: <ErrorBoundaryLayout />,
+    children: [
+      {
+        path: RoutePath.HOME,
+        element: <Home />,
+      },
+      {
+        path: RoutePath.GAME,
+        element: (
+          <ProtectedRoute>
+            <Game />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: RoutePath.SIGN_IN,
+        element: <Login />,
+      },
+      {
+        path: RoutePath.SIGN_UP,
+        element: <Registration />,
+      },
+      {
+        path: RoutePath.LEADER_BOARD,
+        element: (
+          <ProtectedRoute>
+            <LeaderBoard />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: RoutePath.FORUM,
+        element: (
+          <ProtectedRoute>
+            <ForumProvider>
+              <Forum />
+            </ForumProvider>
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: RoutePath.PROFILE,
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: RoutePath.PAGE_NOT_FOUND,
+        element: <NotFoundPage />,
+      },
+      {
+        path: RoutePath.SERVER_ERROR,
+        element: <ServerErrorPage />,
+      },
+    ],
   },
 ]
 
