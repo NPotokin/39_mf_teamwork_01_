213cd96aa8259a6426e04636ace6547302cb767c
Nikita Potokin <116308322+NPotokin@users.noreply.github.com>
2024-06-28 18:31:34 +0300

    Fix/39 38 game mechanics (#20)
    * Убрал некоторые блоки преград
    
    * fix - теперь доступны только 4 кнопки-стрелки клавиатуры
    
    * Починил баг с возможностью продолжения игры после поражения
    
    * add Header and Footer

--- packages/client/src/components/index.ts
+++ packages/client/src/components/index.ts
@@ -1,4 +1,7 @@
 export { GameModal } from './GameModal'
 export { ProtectedRoute } from './ProtectedRoute'
 export { CustomForm } from './CustomForm'
+export { Header } from './Header'
+export { Footer } from './Footer'
 export { ErrorBoundaryLayout } from './ErrorBoundaryLayout'
+
