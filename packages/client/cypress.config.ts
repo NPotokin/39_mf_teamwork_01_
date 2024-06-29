a02c207f50249f378833453153103ccf7beeb63f
KK <holypower77@gmail.com>
2024-06-29 22:27:58 +0300

    feat(cypress): add e2e config

---
+++ packages/client/cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress'
+import vitePreprocessor from 'cypress-vite'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000/',
+    specPattern: './cypress/integration/**/*.spec.{ts,tsx}',
+    setupNodeEvents(on) {
+      on('file:preprocessor', vitePreprocessor())
+    },
+  },
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  pageLoadTimeout: 600000,
+})
