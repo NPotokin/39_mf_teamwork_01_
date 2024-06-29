1fe95f08918e8581d06e06e6e5063ff38e39c66e
OlgaZhyzhka <helgazhyzhka@yandex.kz>
2024-06-29 15:06:45 +0200

    fix: changes methods for update user data

--- packages/client/src/core/api/user.api.ts
+++ packages/client/src/core/api/user.api.ts
@@ -13,18 +13,18 @@ export default class UserApi {
   public updateProfile(
     data: IUpdateUser
   ): Promise<AxiosResponse<IUpdateUserResponse | IAPIError>> {
-    return axiosDB.post<IUpdateUserResponse>('/user/profile', data)
+    return axiosDB.put<IUpdateUserResponse>('/user/profile', data)
   }
 
   public updatePassword(
     data: IUpdatePassword
   ): Promise<AxiosResponse<void | IAPIError>> {
-    return axiosDB.post('/user/password', data)
+    return axiosDB.put('/user/password', data)
   }
 
   public updateAvatar(
     data: FormData
   ): Promise<AxiosResponse<IUserInfo | IAPIError>> {
-    return axiosDB.post('/profile/avatar')
+    return axiosDB.put('/profile/avatar', data)
   }
 }
