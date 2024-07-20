c8f318b42db6f82a709ee6a9ec27dd7abca915ca
OlgaZhyzhka <helgazhyzhka@yandex.kz>
2024-07-20 21:15:38 +0200

    feat: refactor size for game images

--- packages/client/src/components/SectionFeatures/SectionFeatures.tsx
+++ packages/client/src/components/SectionFeatures/SectionFeatures.tsx
@@ -8,22 +8,22 @@ const SectionFeatures = () => (
       </h2>
       <p>
         Step into the paws of a determined panda
-        on a mission to gather all the pumpkins
-        scattered throughout the land! Use the
-        arrow keys to navigate your panda through
-        challenging terrains, but beware – every
-        pumpkin is fiercely guarded by a swift
-        tiger that moves twice as fast and is
-        eager to turn you into its next meal.
+        on a mission to gather all food scattered
+        throughout the land! Use the arrow keys to
+        navigate your panda through challenging
+        terrains, but beware – every pumpkin is
+        fiercely guarded by a swift tiger that
+        moves twice as fast and is eager to turn
+        you into its next meal.
       </p>
       <p>
         To achieve the highest score, you must
-        collect all the pumpkins as quickly as
-        possible while taking the fewest steps.
-        Strategize your moves, outsmart the
-        tigers, and embark on this thrilling
-        adventure. Good luck on your journey, and
-        may your panda's quest be victorious!
+        collect all food as quickly as possible
+        while taking the fewest steps. Strategize
+        your moves, outsmart the enemies, and
+        embark on this thrilling adventure. Good
+        luck on your journey, and may your panda's
+        be victorious!
       </p>
     </div>
   </div>
