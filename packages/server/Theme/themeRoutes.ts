121be53ba9e65304921c661765761b44ffe8288d
evgeniya-burlachenko <e.accounted@gmail.com>
2024-08-12 17:35:35 +0300

    39-79 добавлен lib

--- packages/server/Theme/themeRoutes.ts
+++ packages/server/Theme/themeRoutes.ts
@@ -17,3 +17,24 @@ router.post('/themes/:userId', addTheme)
 router.put('/themes/:userId', updateTheme)
 
 export default router
+
+// import {
+// 	getTheme,
+// 	addTheme,
+// 	updateTheme,
+//   } from './controllers/ThemeApi'
+//   import { Router } from 'express'
+
+//   const themeRouter = Router()
+//   // const router = express.Router()
+
+//   // Получение темы по userId
+//   themeRouter.get('/themes/:userId', getTheme)
+
+//   // Создание новой темы
+//   themeRouter.post('/themes/:userId', addTheme)
+
+//   // Обновление существующей темы
+//   themeRouter.put('/themes/:userId', updateTheme)
+
+//   export default themeRouter
