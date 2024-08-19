e84b36e30db55c0223b43fa490e8cf2348b9ac6d
NPotokin <npotokin@gmail.com>
2024-08-19 12:42:09 +0300

    test fix

--- packages/client/src/pages/Forum/mockData.ts
+++ packages/client/src/pages/Forum/mockData.ts
@@ -11,14 +11,14 @@ export const mockData: Topic[] = [
       {
         commentId: '1',
         content: 'interesting',
-        emoji: '',
+        emojis: [],
         topicId: '1',
       },
-      { commentId: '2', content: 'no bad', emoji: '', topicId: '1' },
+      { commentId: '2', content: 'no bad', emojis: [], topicId: '1' },
       {
         commentId: '3',
         content: 'i like it',
-        emoji: '',
+        emojis: [],
         topicId: '1',
       },
     ],
@@ -30,14 +30,14 @@ export const mockData: Topic[] = [
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     comments: [
-      { commentId: '1', content: 'ok', emoji: '', topicId: '2' },
+      { commentId: '1', content: 'ok', emojis: [], topicId: '2' },
       {
         commentId: '2',
         content: 'chudesno',
-        emoji: '',
+        emojis: [],
         topicId: '2',
       },
-      { commentId: '3', content: 'super', emoji: '', topicId: '2' },
+      { commentId: '3', content: 'super', emojis: [], topicId: '2' },
     ],
   },
   {
@@ -50,11 +50,11 @@ export const mockData: Topic[] = [
       {
         commentId: '1',
         content: 'i love it',
-        emoji: '',
+        emojis: [],
         topicId: '3',
       },
-      { commentId: '2', content: 'its ok', emoji: '', topicId: '3' },
-      { commentId: '3', content: 'eee', emoji: '', topicId: '3' },
+      { commentId: '2', content: 'its ok', emojis: [], topicId: '3' },
+      { commentId: '3', content: 'eee', emojis: [], topicId: '3' },
     ],
   },
   {
@@ -63,9 +63,9 @@ export const mockData: Topic[] = [
     count: '3',
     description: 'This is the content of Technologies topic',
     comments: [
-      { commentId: '1', content: 'ok', emoji: '', topicId: '4' },
-      { commentId: '2', content: 'eee', emoji: '', topicId: '4' },
-      { commentId: '3', content: 'amazing', emoji: '', topicId: '4' },
+      { commentId: '1', content: 'ok', emojis: [], topicId: '4' },
+      { commentId: '2', content: 'eee', emojis: [], topicId: '4' },
+      { commentId: '3', content: 'amazing', emojis: [], topicId: '4' },
     ],
   },
 ]
