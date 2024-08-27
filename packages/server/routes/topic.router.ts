e886a7fd1b69382705e555cf614dc51fa62c000e
Nikita Potokin <116308322+NPotokin@users.noreply.github.com>
2024-08-27 14:45:47 +0300

    Feat/39 87 xss protection (#69)
    * Inintial set up and usage of XSS validator at index.ts
    
    * Removed helmet, moved xss validator from global to potentially dangerous routes
    
    * fix

--- packages/server/routes/topic.router.ts
+++ packages/server/routes/topic.router.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express'
 import { topicController } from '../controllers'
 import { topicCreateDataValidate, topicUpdateDescDataValidate } from '../validation'
+import { xssValidator } from '../middleware/xssValidation'
 
 const topicRouter = Router()
 
 topicRouter.get('/topics', topicController.getAllTopics)
 topicRouter.get('/topics/:id', topicController.getTopicById)
 
-topicRouter.post('/topics', topicCreateDataValidate, topicController.createTopic)
+topicRouter.post('/topics', xssValidator(), topicCreateDataValidate, topicController.createTopic)
 
-topicRouter.put('/topics/:id', topicUpdateDescDataValidate, topicController.updateTopicDescription)
+topicRouter.put(
+  '/topics/:id',
+  xssValidator(),
+  topicUpdateDescDataValidate,
+  topicController.updateTopicDescription
+)
 
 topicRouter.delete('/topics/:id', topicController.deleteTopic)
 
